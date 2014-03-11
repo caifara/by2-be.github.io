db5672dac37d71068bcbf4cda6aa8de51765c733
Ivo Dancet <ivo.dancet@gmail.com>
2014-03-11 13:04:43 +0100

    mobiele browsers tonen best geen filmpjes of parallax effecten

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -1,6 +1,9 @@
 $(function(){
-  $(".player").mb_YTPlayer();
-  $(window).stellar();
+  var desktop_browser = window.matchMedia("(min-width: 980px)").matches;
+  if(desktop_browser){
+    $(".player").mb_YTPlayer();
+    $(window).stellar();
+  }
 
   if(window.location.hash == '#bedankt'){
     $('#thank-you').removeClass("hidden");
